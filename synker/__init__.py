eacf356e4b0fe5577cac6ed7d6574ea41c4683ee
kavehkm <mehrbaniankaveh@gmail.com>
2020-11-01 09:10:20 +0330

    replace Exception with EXXX(Synker Base Exception) and also add some validations to local_directory.setter and cloud_directory.setter

--- synker/__init__.py
+++ synker/__init__.py
@@ -1,5 +1,7 @@
 # standard
+import os
 # internal
+from synker.exceptions import EXXX
 from synker.providers import DropboxProvider
 from synker.directories import CloudDirectory, LocalDirectory
 # external
@@ -13,15 +15,27 @@ class Synker(object):
     @property
     def cloud_directory(self):
         if self._cloud_directory is None:
-            raise Exception('cloud directory is not set')
+            raise EXXX('cloud is not set')
         return self._cloud_directory
 
     @cloud_directory.setter
     def cloud_directory(self, cs):
+        error = ''
         cloud_path = cs.get('cloud_path')
         cloud_limit = cs.get('cloud_limit')
         mtime_file = cs.get('mtime_file')
         token = cs.get('token')
+        # validation
+        if not cloud_path:
+            error = 'invalid cloud path'
+        elif not cloud_limit or not isinstance(cloud_limit, int):
+            error = 'invalid cloud limit'
+        elif not mtime_file:
+            error = 'invalid mtime file'
+        elif not token:
+            error = 'invalid token'
+        if error:
+            raise EXXX(error)
         dp = DropboxProvider()
         dp.set_token(token)
         cd = CloudDirectory(dp, cloud_path, cloud_limit, mtime_file)
@@ -30,13 +44,21 @@ class Synker(object):
     @property
     def local_directory(self):
         if self._local_directory is None:
-            raise Exception('local directory is not set')
+            raise EXXX('local directory is not set')
         return self._local_directory
 
     @local_directory.setter
     def local_directory(self, ls):
+        error = ''
         directory_path = ls.get('directory_path')
         pattern = ls.get('pattern')
+        # validation
+        if not pattern:
+            error = 'invalid pattern'
+        elif not os.access(directory_path, 7):
+            error = 'access denied for directory: {}'.format(directory_path)
+        if error:
+            raise EXXX(error)
         ld = LocalDirectory(directory_path, pattern)
         self._local_directory = ld
 

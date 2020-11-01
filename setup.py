82677bb8d9ec5607523ff438166e203e71146495
kavehkm <mehrbaniankaveh@gmail.com>
2020-11-01 10:35:14 +0330

    create setup.py and setup.cfg to publish synker on pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+  name='synker',
+  packages=['synker'],
+  version='0.1',
+  license='Apache',
+  description='backup tool',
+  author='kaveh mehrbanian',
+  author_email='mehrbaniankaveh@gmail.com',
+  url='https://github.com/kavehkm/Synker',
+  download_url='https://github.com/kavehkm/Synker/archive/v1.0.tar.gz',
+  keywords=['backup'],
+  install_requires=['requests']
+)
